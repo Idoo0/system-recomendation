80c12b1312e208b8991f87b277e8102fde84086b
idoo0 <ridhoaditya925@gmail.com>
2024-09-19 16:08:25 +0700

    feat: add new route

--- index.py
+++ index.py
@@ -1,4 +1,9 @@
+import flag
 from fastapi import FastAPI
 from routes.update import update
+from routes.recomendation import recomendation
+
 app = FastAPI()
+
 app.include_router(update)
+app.include_router(recomendation)
